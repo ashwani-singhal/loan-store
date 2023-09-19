b526a6809f258faa4b035cbca77bd2b4b861ae0f
ashwani.singhal <ashwani.singhal@hashedin.com>
2023-09-19 20:35:08 +0530

    Exception handling added for controller class

---
+++ src/main/java/com/loanstore/loanstore/exceptions/IlligalArgument.java
@@ -0,0 +1,9 @@
+package com.loanstore.loanstore.exceptions;
+
+public class IlligalArgument extends RuntimeException{
+
+    public IlligalArgument(String message){
+        super(message);
+    }
+
+}
